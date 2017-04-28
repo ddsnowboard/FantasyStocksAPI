569a5360b3ddb8d62c39982f11404d0f2c30084d
Will Koster <jameswk2@illinois.edu>
2017-04-28 14:20:41 -0500

    Fix typo

--- FantasyStocksAPI/src/com/jameswk2/FantasyStocksAPI/User.java
+++ FantasyStocksAPI/src/com/jameswk2/FantasyStocksAPI/User.java
@@ -18,7 +18,7 @@ their respective caches.
 Although another option would be to dispense with Abbreviated* and Full* altogether and just have everything be a full
 model. The abbreviated versions would still exist but they would just be to read the Json and they would immediately be replaced
 with real versions. The user would never see partial versions. That would be a whole rewrite, and I'm not even sure
-if eithe of these plans would actually work properly. This is an annoying problem. Maybe I can solve it all in Android.
+if either of these plans would actually work properly. This is an annoying problem. Maybe I can solve it all in Android.
 There has to be a way to have dynamic arrays in Tabviews or whatever. I mean that's ridiculous. Maybe I pass in the
 Floor and then the tabView gets the info itself. Hmmm....
  */
