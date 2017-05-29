d40df29abaee68781c4068682b79a90dd5294e98
Will Koster <jameswk2@illinois.edu>
2017-05-29 11:31:13 -0500

    Document the Floor class
    Now I just have to do all the other public methods. NBD.

--- FantasyStocksAPI/src/com/jameswk2/FantasyStocksAPI/Floor.java
+++ FantasyStocksAPI/src/com/jameswk2/FantasyStocksAPI/Floor.java
@@ -6,27 +6,64 @@ import static com.jameswk2.FantasyStocksAPI.FullFloor.MODEL_NAME;
  * Created by ddsnowboard on 4/17/17.
  */
 public interface Floor {
+
+    /**
+     * @param id the integral id associated with the desired {@link Floor} object
+     * @return the floor assocaited with the given id
+     */
     static Floor get(int id) {
         return (FullFloor) Model.getModel(id, MODEL_NAME, FullFloor.class);
     }
 
+    /**
+     * @return the integer id associated with this object
+     */
     int getId();
 
+    /**
+     * @return the name of this {@link Floor} object
+     */
     String getName();
 
+    /**
+     * @return an array of the {@link Stock} objects on this {@link Floor}
+     */
     Stock[] getStocks();
 
+    /**
+     * @return a {@link FullFloor.Permissiveness} corresponding to the permissiveness of this object
+     */
     FullFloor.Permissiveness getPermissiveness();
 
+    /**
+     * @return a {@link User} object representing the owner of this object
+     */
     User getOwner();
 
+    /**
+     * Returns the floor player assigned to this floor. The floor player is the virtual player that
+     * holds all the stocks that no other player on the Floor is holding. He does have a score in the 
+     * database, but it is meaningless, and whether or not it is returned through this API is undefined.
+     * @return a {@link Player} object representing the floor player on this {@link Floor}
+     */
     Player getFloorPlayer();
 
+    /**
+     * @return whether the floor is public or not
+     */
     boolean isPublic();
 
+    /**
+     * @return the number of stocks available on this floor.
+     */
     int getNumStocks();
 
-    static Floor[] getFloors() {
-        return (Floor[]) Model.getModel(MODEL_NAME, FullFloor.class);
-    }
+    /**
+     * Returns all the floors in the database, subject to certain constraints,
+     * specifically that the floor is either public or the user is a member.
+     * @return all the {@link Floor}s in the database that are available to the user
+     */
+     static Floor[] getFloors() {
+     return (Floor[]) Model.getModel(MODEL_NAME, FullFloor.class);
+     }
 }
